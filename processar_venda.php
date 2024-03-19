83f0cd7b273e28e8d9ab73e81c3ccb7d476694a4
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-19 11:06:42 -0300

    Ajuste de processa_cadastro.php

--- processar_venda.php
+++ processar_venda.php
@@ -21,11 +21,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->begin_transaction();
 
     // Consulta SQL para obter a quantidade atual do produto
-    $sql_select = "SELECT quantidade FROM produtos WHERE id = ?";
+    $sql_select = "SELECT quantidade, numeroserie FROM produtos WHERE id = ?";
     $stmt_select = $conn->prepare($sql_select);
     $stmt_select->bind_param("i", $produto_id);
     $stmt_select->execute();
-    $stmt_select->bind_result($quantidade);
+    $stmt_select->bind_result($quantidade, $produto_numeroserie);
     $stmt_select->fetch();
     $stmt_select->close();
 
@@ -35,14 +35,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nova_quantidade = $quantidade - $quantidade_vendida; // Reduz a quantidade em quantidade_vendida
         $sql_update = "UPDATE produtos SET quantidade = ? WHERE id = ?";
         $stmt_update = $conn->prepare($sql_update);
-        $stmt_update->bind_param("ii", $nova_quantidade, $produto_id,);
+        $stmt_update->bind_param("ii", $nova_quantidade, $produto_id);
         $stmt_update->execute();
         $stmt_update->close();
 
         // Insere os detalhes da venda na tabela de log de vendas
-        $sql_insert = "INSERT INTO log_vendas (produto_id, quantidade_vendida, comprador, ra, forma_pagamento, observacao, produto_numeroserie) VALUES (?, ?, ?, ?, ?, ?,?)";
+        $sql_insert = "INSERT INTO log_vendas (produto_id, quantidade_vendida, comprador, ra, forma_pagamento, observacao, produto_numeroserie) VALUES (?, ?, ?, ?, ?, ?, ?)";
         $stmt_insert = $conn->prepare($sql_insert);
-        $stmt_insert->bind_param("iissss", $produto_id, $quantidade_vendida, $comprador, $ra, $forma_pagamento, $observacao, $produto_numeroserie); // Corrigido o tipo de dado para comprador
+        $stmt_insert->bind_param("iisssss", $produto_id, $quantidade_vendida, $comprador, $ra, $forma_pagamento, $observacao, $produto_numeroserie);
         $stmt_insert->execute();
         $stmt_insert->close();
 
@@ -59,4 +59,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 // Fechar conexão
 $conn->close();
-
+?>
