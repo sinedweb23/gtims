f466009075129ac7048b3c5e56c9614cdbd175e9
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-19 10:54:28 -0300

    Ajuste de processa_cadastro.php

--- baixa_venda.php
+++ baixa_venda.php
@@ -71,7 +71,7 @@ $resultado = $conn->query($sql);
             <?php
             // Loop através dos resultados da consulta para exibir os produtos em um menu suspenso
             while ($row = $resultado->fetch_assoc()) {
-                echo "<option value='" . $row['id'] . "'>" . $row['nome'] . " (N. Serie: " . $row['numeroserie'] . ")</option>";
+                echo "<option value='" . $row['id'] . "'>" . $row['nome'] . " (N. Série: " . $row['numeroserie'] . ")</option>";
             }
             ?>
         </select><br>
