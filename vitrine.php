74a9fdc4d2c583c175d22bfde3ae8878a12c1b27
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-22 16:14:43 -0300

    Implmentação da vitrine

--- vitrine.php
+++ vitrine.php
@@ -84,7 +84,8 @@
         <img src="banner.png" alt="Banner" class="banner">
         <h1>Desapegos de TI Morumbi Sul</h1>
         <p>Explore nossa seleção de equipamentos usados.</p>
-        <p>Por favor, note que todos os equipamentos são vendidos sem garantia, mas garantimos que cada item foi cuidadosamente inspecionado para garantir sua funcionalidade e qualidade. Sinta-se à vontade para navegar em nossa vitrine e encontrar o equipamento perfeito para você.</p>
+        <p>Por favor, note que todos os equipamentos são vendidos sem garantia, mas garantimos que cada item foi cuidadosamente inspecionado para garantir sua funcionalidade e qualidade. Sinta-se à vontade para navegar em nossa vitrine e encontrar o equipamento perfeito para você.</p><BR>
+        <p>NOVIDADES EM DESTAQUE</p>
         <div class="produtos">
             <?php
                 require_once 'config.php'; // Arquivo de conexão com o banco de dados
