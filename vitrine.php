59258c9b123cc391943f53677ffb1dd2bcfacb44
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-25 12:45:28 -0300

    Atualição da versão

--- vitrine.php
+++ vitrine.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desapegos de TI Morumbi Sul.</title>
+    <title>Desapegos de TI Morumbi Sul</title>
     <style>
         body {
             font-family: Arial, sans-serif;
