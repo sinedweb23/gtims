58ba9f6fa5b0e759de5be2e57738385c393e7166
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-25 12:50:24 -0300

    Atualição da versão

--- vitrine.php
+++ vitrine.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desapegos de TI Morumbi Sul..</title>
+    <title>Desapegos de TI Morumbi Sul</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -82,7 +82,7 @@
 <body>
     <div class="container">
         <img src="banner.png" alt="Banner" class="banner">
-        <h1>Desapegos de TI Morumbi Sul</h1>
+        <h1>Desapegos de TI Morumbi Sul..</h1>
         <p>Explore nossa seleção de equipamentos usados.</p>
         <p>Por favor, note que todos os equipamentos são vendidos sem garantia, mas garantimos que cada item foi cuidadosamente inspecionado para garantir sua funcionalidade e qualidade. Sinta-se à vontade para navegar em nossa vitrine e encontrar o equipamento perfeito para você.<br><br>
             Formas de pagamento: Cartão, dinheiro, pix e desconto em folha.
