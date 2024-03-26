062cb3ee3e89f048a3942aa9379d1534cf553797
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-26 14:12:57 -0300

    teste 3 Script

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $gitVersion = getGitVersion();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Gestão de TI Morumbi Sul</title>
+    <title>Sistema de Gestão de TI Morumbi Sul.</title>
     <style>
         body {
             font-family: Arial, sans-serif;
