fc2425fd156cca158cb4c882b8ae49fd955ddcb8
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-26 14:47:27 -0300

    teste

--- update.sh
+++ update.sh
@@ -41,6 +41,7 @@ git push origin main --tags
 #ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
 #ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo git checkout \"$VERSION\" && sudo git push origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
 #ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo git checkout \"$VERSION\ && sudo git push origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
-ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo git checkout \"$VERSION\" && sudo git push origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
+#ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo git checkout \"$VERSION\" && sudo git push origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
+ssh -i "$CHAVE_SSH" ubuntu@glpi.morumbisul.com.br "cd $LINUX_CODE_DIR && sudo git pull origin main && sudo git checkout tags/$VERSION && sudo git push origin main && sudo chown www-data.www-data -R $LINUX_CODE_DIR"
 
 
