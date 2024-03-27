aca63a1c7938af0bd746e6715700772893d48959
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-03-27 14:47:30 -0300

    mostrar versão

--- index.php
+++ index.php
@@ -147,4 +147,7 @@ $gitVersion = getGitVersion();
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+<footer>
+<?php echo $gitVersion; ?>
+</footer>
 </html>
