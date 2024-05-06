b0fd762c7ea946c449dbf0a4157e4d8fa97f1ccd
sinedweb23 <denis.objetivo.jar@gmail.com>
2024-05-06 08:36:23 -0300

    Correçã oabertura de chamado

--- chamado/fechar_chamado.php
+++ chamado/fechar_chamado.php
@@ -1,7 +1,6 @@
 <?php
 // Inclui o arquivo de configuração do banco de dados
 require_once('config1.php');
-session_start();
 
 // Verifica se os dados do formulário foram enviados
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['chamado_id']) && isset($_POST['solucao'])) {
@@ -11,11 +10,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['chamado_id']) && isset
     // Obtém a data e hora atual
     $data_fechamento = date('Y-m-d H:i:s');
     
-    // Obtém o email do usuário logado
-    $usuario_fechamento = $_SESSION['email'];
-    
-    // Atualiza o status do chamado para "Fechado", a data de fechamento, a solução e o usuário que fechou o chamado no banco de dados
-    $sql = "UPDATE chamados SET status = 'Fechado', data_fechamento = '$data_fechamento', solucao = '$solucao', usuario = '$usuario_fechamento' WHERE id = $chamado_id";
+    // Atualiza o status do chamado para "Fechado", a data de fechamento e a solução no banco de dados
+    $sql = "UPDATE chamados SET status = 'Fechado', data_fechamento = '$data_fechamento', solucao = '$solucao' WHERE id = $chamado_id";
     if ($conn->query($sql) === TRUE) {
         // Retorna uma resposta JSON indicando sucesso
         echo json_encode(array('success' => true));
